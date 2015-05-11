0c9980748b910c811e3f21c3815b47e6959feee1
shmulim <shmuli@Shmulis-MacBook-Pro.local>
2015-05-11 01:27:43 -0700

    Reactive ToDo List Example in MeteorJS

---
+++ simple-todos.js
@@ -0,0 +1,35 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+}
